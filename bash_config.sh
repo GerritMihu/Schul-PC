92e16ceb0f15591900dcfbdb6a7ebeb47edd5af7
Gerrit Mitterhuemer <gerritmihu@gmail.com>
2026-02-19 17:32:40 +0100

    Neue config für bash geladen

---
+++ bash_config.sh
@@ -0,0 +1,12 @@
+!# /bin/bash
+
+
+# install nerd fonts
+
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip
+unzip JetBrainsMono.zip
+fc-cache -fv
+
+
